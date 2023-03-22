8801057804f8f9c15fb03aa745ae9eb8d1d2cd4c
igol84 <igolub84@gmail.com>
2023-03-22 17:28:56 +0200

    items editor translate

--- src/pages/items-editor/index.tsx
+++ src/pages/items-editor/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Header from "../../components/Header";
 import {Box} from "@mui/material";
-import {useDictionary} from "../../hooks/pages";
+import {useDictionary, useDictionaryTranslate} from "../../hooks/pages";
 import ItemsEdit from "../../components/items-editor/ItemsEditor";
 
 
 
 const Items = () => {
-  // const d = useDictionary('places')
+  const dict = useDictionaryTranslate('itemsEditor')
   return (
     <Box m={1}>
       <Box display='flex' justifyContent='space-around' alignItems='center'>
-        <Header title={'Items Editor'}/>
+        <Header title={dict('title')}/>
       </Box>
       <ItemsEdit/>
     </Box>
